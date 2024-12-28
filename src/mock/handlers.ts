7bf2678899a7c046f3e69c0413b65544deb26891
張祝維 <chuweichang0319@gmail.com>
2024-12-28 22:32:35 +0800

    adjust key to header

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -4,6 +4,14 @@ interface LoginRequest {
   user: string;
   password: string;
 }
+interface RegisterRequest {
+  title:string;
+  deadline:string;
+  finish:boolean;
+  priority:number;
+}
+let sessionKey = 'Bearer 12345678';
+
 
 export const handlers = [
 
@@ -14,7 +22,14 @@ export const handlers = [
 
       if (user === 'user' && password === 'password') {
         console.log('Login successful');
-        return HttpResponse.json({ message: 'Login successful', sessionKey: "123" });
+        return HttpResponse.json(
+          { message: 'Login successful' },
+          {
+            headers: {
+              'Authorization': sessionKey,
+            },
+          }
+        );
       } else {
         console.log('Login failed');
         return HttpResponse.json(
@@ -31,4 +46,29 @@ export const handlers = [
       );
     }
   }),
+  // http.post('/api/add', async ({ request }) => {
+  //   try {
+  //     const body:RegisterRequest = (await request.json()) as RegisterRequest;  //would probably crash if additional field provided
+  //     const authHeader = request.headers.get('Authorization');
+  //     const { title, deadline, finish, priority } = body;
+  //
+  //     if (user === 'user' && password === 'password') {
+  //       console.log('Login successful');
+  //       return HttpResponse.json({ message: 'Login successful', sessionKey: "123" });
+  //     } else {
+  //       console.log('Login failed');
+  //       return HttpResponse.json(
+  //         { error: 'Invalid username or password' },
+  //         { status: 401 } // Simulate an HTTP 401 Unauthorized response
+  //       );
+  //     }
+  //   }catch (error) {
+  //     // Handle error if the body cannot be parsed as JSON
+  //     console.error('Failed to parse JSON:', error);
+  //     return HttpResponse.json(
+  //       { error: 'Invalid request data' },
+  //       { status: 400 } // Return a 400 Bad Request if parsing fails
+  //     );
+  //   }
+  // }),
 ]
