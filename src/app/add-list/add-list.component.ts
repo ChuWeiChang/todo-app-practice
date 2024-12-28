de7ea40f83530bb76a963d792df6f2f4b4a0fafb
張祝維 <chuweichang0319@gmail.com>
2024-12-28 21:14:16 +0800

    define doms

--- src/app/add-list/add-list.component.ts
+++ src/app/add-list/add-list.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {FormArray, FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-add-list',
@@ -10,11 +11,12 @@ import {FormArray, FormBuilder, ReactiveFormsModule, Validators} from '@angular/
   template:
     `
       <button type="button" (click)="addItem()">Add Row</button>
-      <button type="submit" [disabled]="!todoListForm.valid">Submit</button>
       <form [formGroup]="todoListForm" (ngSubmit)="onSubmit()">
-        <div formArrayName="todoListItems">
+        <button type="submit" [disabled]="!todoListForm.valid">Submit</button>
+        <div class="form-container">
+          <div formArrayName="todoListItems">
           @for(item of todoListItems.controls; let i = $index ;track i){
-            <div class="form">
+            <div [formGroupName]="i">
               <label for="title-{{ i }}">Title: </label>
               <input id="title-{{ i }}" type="text" formControlName="title">
 
@@ -30,13 +32,13 @@ import {FormArray, FormBuilder, ReactiveFormsModule, Validators} from '@angular/
               <button type="button" (click)="removeTodoItem(i)">Remove</button>
             </div>
           }
-
+          </div>
         </div>
       </form>
   `,
   styles:
     `
-      [formArrayName]{
+      .form-container{
         display: flex;
         flex-direction: column;
         align-items: flex-start;
@@ -46,6 +48,7 @@ import {FormArray, FormBuilder, ReactiveFormsModule, Validators} from '@angular/
 })
 export class AddListComponent {
   private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
 
   todoListForm = this.fb.group({
     todoListItems: this.fb.array([])
@@ -57,7 +60,7 @@ export class AddListComponent {
   addItem(){
     const todoListItem = this.fb.group({
       title: ['', [Validators.required]],
-      deadline: ['', [Validators.required]],
+      deadline: [new Date().toString(), [Validators.required]],
       finished: [false],
       priority: [0, [Validators.required]],
     });
@@ -68,7 +71,17 @@ export class AddListComponent {
     this.todoListItems.removeAt(index);
   }
 
-  onSubmit() {
-
+  onSubmit(): void {
+    console.log("fewfewf");
+    const payload = this.todoListForm.value;
+    this.http.post('/api/add', payload).subscribe({
+      next: (response) => {
+        console.log('Form submitted successfully:', response);
+        this.todoListForm.reset();
+      },
+      error: (error) => {
+        console.error('Error submitting form:', error);
+      }
+    });
   }
 }
