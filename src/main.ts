821f747512b497976bcb49be5e162be1d3bf70f7
ChuWeiChang <39853288+chuweichang@users.noreply.github.com>
2024-12-28 10:46:02 +0800

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
+const { server } = await import('./mock/mock-server');
+server.listen();
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
