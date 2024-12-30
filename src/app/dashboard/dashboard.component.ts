830d682058bcbc4a18cac826a0bd7933142d68dd
張祝維 <chuweichang0319@gmail.com>
2024-12-30 21:00:11 +0800

    refactor cdk to their own component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,10 +13,8 @@ import {
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {LoginStateService} from '../login-state.service';
 import {Router} from '@angular/router';
-import {MatDialog, MatDialogActions, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
-import {MatButton} from '@angular/material/button';
-import {DIALOG_DATA, DialogRef} from '@angular/cdk/dialog';
-import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
+import {QuickAddComponent} from '../quick-add-panel/quick-add-panel.component';
 
 export interface TodoListItem {
   title: string;
@@ -146,80 +144,4 @@ export class ExampleDataSource extends DataSource<TodoListItem> {
   }
 }
 
-@Component({
-  selector: 'app-quick-add-panel',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    MatDialogContent,
-    MatDialogActions,
-    MatButton,
-    MatDialogTitle,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  template: `
-    <h2 mat-dialog-title>{{ data.title }}</h2>
-    <mat-dialog-content>
-      <div class="form-container">
-        <form class="formGroup" [formGroup]="newTodoForm" (ngSubmit)="onSubmit()">
-          <label for="title">Title:</label>
-          <input id="title" type="text" formControlName="title">
 
-          <label for="deadline">Deadline: </label>
-          <input id="deadline" type="datetime-local" formControlName="deadline">
-
-          <label for="finished">Finished: </label>
-          <input id="finished" type="checkbox" formControlName="finished">
-
-          <label for="priority">Priority: </label>
-          <input id="priority" type="number" formControlName="priority" min="0">
-
-          <button type="submit" [disabled]="!newTodoForm.valid">Add Todo</button>
-        </form>
-      </div>
-    </mat-dialog-content>
-    <mat-dialog-actions>
-      <button mat-button (click)="dialogRef.close()">Close</button>
-    </mat-dialog-actions>
-  `,
-  styles:`
-    .formGroup{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 5px;
-    }
-  `
-})
-export class QuickAddComponent {
-  dialogRef = inject<DialogRef<string>>(DialogRef<string>);
-  data = inject(DIALOG_DATA);
-  fb = inject(FormBuilder);
-  http = inject(HttpClient);
-  newTodoForm = this.fb.group({
-    title: ['', [Validators.required]],
-    deadline: [new Date().toISOString().slice(0, 16), [Validators.required]],
-    finished: [false],
-    priority: [0, [Validators.required]],
-  });
-  onSubmit(){
-    const formData = this.newTodoForm.value;
-    const headers = new HttpHeaders().set('Authorization', this.data.sessionKey);
-    this.http.post('/api/append-list', formData, { headers}).subscribe({
-      next: () => {
-        alert("successfully add new item");
-        this.newTodoForm.reset({
-          title: '',
-          deadline: new Date().toISOString().slice(0, 16),
-          finished: false,
-          priority: 0,
-        });
-      },
-      error: (err) => {
-        alert("add new item failed");
-        console.error('Error adding new item:', err);
-      },
-    });
-  }
-
-}
