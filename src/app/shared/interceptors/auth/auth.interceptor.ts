7b093d7544b42be1c2fe6d2b8c9246831d6e6ed0
張祝維 <chuweichang0319@gmail.com>
2024-12-31 16:18:24 +0800

    apply interceptor

--- src/app/shared/interceptors/auth/auth.interceptor.ts
+++ src/app/shared/interceptors/auth/auth.interceptor.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {LoginStateService} from '../../services/auth/login-state.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private loginState: LoginStateService) {}
+  private loginState = inject(LoginStateService)
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const sessionKey = this.loginState.sessionKey();
