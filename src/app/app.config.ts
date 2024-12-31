c9872bb783995cc39807a28aa0cabdabe91ac9c9
張祝維 <chuweichang0319@gmail.com>
2024-12-31 16:12:08 +0800

    apply interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {AuthInterceptor} from './shared/interceptors/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient( withFetch()),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync()]
+  providers: [provideHttpClient(
+    withInterceptorsFromDi(),
+  ),
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    provideHttpClient( withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync()]
 };
--- src/app/shared/interceptors/auth/auth.interceptor.ts
+++ src/app/shared/interceptors/auth/auth.interceptor.ts
@@ -1,5 +1,25 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {LoginStateService} from '../../services/auth/login-state.service';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private loginState: LoginStateService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const sessionKey = this.loginState.sessionKey();
+
+    if (sessionKey) {
+      const clonedRequest = req.clone({
+        setHeaders: {
+          Authorization: sessionKey
+        }
+      });
+
+      return next.handle(clonedRequest);
+    } else {
+      return next.handle(req);
+    }
+  }
+}
